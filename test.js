12c6c816714cf23e1bad0d3b2977749286882c04
ChibaConnor <nattawatwichaiyo1@gmail.com>
2023-07-11 11:59:38 +0700

    update

--- test.js
+++ test.js
@@ -1,9 +1,21 @@
-var t = setTimeout(function() {
-  console.log('test1')
-}, 1000)
-console.log('test2')
-clearTimeout(t)
+// var t = setTimeout(function() {
+//   console.log('test1')
+// }, 1000)
+// console.log('test2')
+// clearTimeout(t)
 
 // git config --global user.name "ChibaConnor"
 
 // git config --global user.email"nattawatwichaiyo1@gmail.com"
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
