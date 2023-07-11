f39b8b0173108700a26478091574bb9f9141d0db
ChibaConnor <nattawatwichaiyo1@gmail.com>
2023-07-11 10:47:22 +0700

    -

---
+++ test.js
@@ -0,0 +1,9 @@
+var t = setTimeout(function() {
+  console.log('test2')
+}, 1000)
+console.log('test1')
+clearTimeout(t)
+
+// git config --global user.name "ChibaConnor"
+
+// git config --global user.name "nattawatwichaiyo1@gmail.com"
